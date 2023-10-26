2f23a2b9749564a939e35ca9e1000c1c07373c5f
Ezhaym N <ezhaym@gmail.com>
2023-10-25 22:23:15 -0600

    First commit

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>{{ $title ?? 'Nájera' }}</title>
+
+        @vite('resources/css/app.css')
+        @livewireStyles
+    </head>
+    <body>
+
+        <main>
+            <div class="container">
+                {{ $slot }}
+            </div>
+        </main>
+
+        @vite('resources/js/app.js')
+        @livewireStyles
+    </body>
+</html>
